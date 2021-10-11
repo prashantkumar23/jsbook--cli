e5b87cbd997c208add3bf075dbc41731e314e446
Prashant Kumar <prashantdrew23@gmail.com>
2021-10-11 14:53:52 +0530

    updated readme

--- notebook.js
+++ notebook.js
@@ -1,1 +1,1 @@
-[{"content":"show(<h1>Hi therlskndlnsale</h1>)","type":"code","id":"j1b5k"},{"content":"Some text!","type":"text","id":"kkgj1"}]
+[{"content":"# Welcome to my notebook\n\n\nHere is an example of a React component","type":"text","id":"3p4is"},{"content":"import { useState } from 'react';\n\nconst Counter = () => {\n  const [count, setCounter] = useState(0);\n\n  return <div>\n    <button onClick={() => setCounter(count + 1)}>Increase</button>\n    <h3> Count {count}</h3>\n  </div>;\n};\n\nshow(<Counter/>)","type":"code","id":"k4hd2"}]
